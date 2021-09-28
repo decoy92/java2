1d4f634b83e189dfa3fd659368c513a8d88887e1
Heisenbug <83627908+decoy92@users.noreply.github.com>
2021-09-28 23:51:56 +0900

    Add files via upload

---
+++ Operator/OperatorEx1.java
@@ -0,0 +1,13 @@
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		i++; // i=i+1; 과 같은 의미. ++i로 바꿔도 결과는 같다.
+		System.out.println(i);
+		i=5;
+		++i;
+		System.out.println(i);		
+	}
+
+}
